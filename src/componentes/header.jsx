b754b3e5cdcaf6f9a2573664c0cb9ed1af525605
melhdzn <115022707+melhdzn@users.noreply.github.com>
2022-10-24 01:09:58 -0500

    cambios

--- src/componentes/header.jsx
+++ src/componentes/header.jsx
@@ -1,7 +1,7 @@
-function Header(){
-    
-    
-    return(
+function Header() {
+
+
+    return (
         <h1 className="font-black text-center mx-auto mt-10 text-5xl md:w-2/3 ">Seguimiento de Pacientes <span className="text-indigo-500 ">Veterinaria</span></h1>
     )
 }
